024f095518e7ce5319817b86432a126e1e2c24a3
Maksim Belov <45949002+artdeell@users.noreply.github.com>
2026-04-14 18:59:18 +0300

    Reenable rpmalloc

--- ci_build_all.bash
+++ ci_build_all.bash
@@ -54,7 +54,7 @@ if [[ "$LWJGL_VERSION" == "3.2.3" || "$LWJGL_VERSION" == "3.3.1" ]]; then
    apply_patch lwjgl3_3.2.3_use_packaging_arch "fix offline build natives arch"
 fi
 
-export ANTFLAGS="-lib $NASHORN -Dplatform.linux=true -Dbinding.nfd=false -Dbinding.jawt=false -Dbinding.remotery=false -Dbinding.zstd=false -Dbinding.rpmalloc=false -Dbinding.yoga=false -Dbinding.meow=false"
+export ANTFLAGS="-lib $NASHORN -Dplatform.linux=true -Dbinding.nfd=false -Dbinding.jawt=false -Dbinding.remotery=false -Dbinding.zstd=false -Dbinding.yoga=false -Dbinding.meow=false"
 
 ant $ANTFLAGS compile-templates compile
 
